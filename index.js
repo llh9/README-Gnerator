ca8edf8104871e268ab1be9de126462a05bb7bf6
Montii <llh9@yahoo.com>
2021-10-31 14:55:42 -0400

    video link used instead

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+// include inquirer and file system i/o
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+//use the prompt method with input and checkbox type questions and default values 
 inquirer.prompt([
 
     {
@@ -37,8 +39,8 @@ inquirer.prompt([
         type: "checkbox",
         message: "check all items you want to include in your README",
         name: "Table_of_contents",
-        default: ["Installation", "Usage", "License", "Contributors", "Test", "Questions"],
-        choices: ["Installation", "Usage", "License", "Contributors", "Test", "Questions"],
+        default: ["Installation", "Usage", "License", "Contributors", "Test", "Questions", "Demo", "Badges", "Features", "Contribute"],
+        choices: ["Installation", "Usage", "License", "Contributors", "Test", "Questions", "Demo", "Badges", "Features", "Contribute"],
     },
     {
         type: "input",
@@ -83,7 +85,7 @@ inquirer.prompt([
     console.log(answers);
 
     fs.writeFile(`README.MD`, `# ${answers.title}\n
-    \n## Description
+    \n## [Description](#description)
     \n${answers.description}
     \n### Motivation
     \n${answers.motivation}
@@ -91,35 +93,40 @@ inquirer.prompt([
     \n${answers.solutions}
     \n### What I learned 
     \n${answers.lessons}
-    \n## Table of Contents\n 
+    \n## [Table of Contents](#table-of-contents)\n 
     \n1. [${answers.Table_of_contents[0]}](#installation)
     \n1. [${answers.Table_of_contents[1]}](#usage)
-    \n1. [${answers.Table_of_contents[2]}](#license)
-    \n1. [${answers.Table_of_contents[3]}](#contributors)
+    \n1. [${answers.Table_of_contents[6]}](#demo)
     \n1. [${answers.Table_of_contents[4]}](#test)
+    \n1. [${answers.Table_of_contents[7]}](#badges)
+    \n1. [${answers.Table_of_contents[8]}](#features)
+    \n1. [${answers.Table_of_contents[3]}](#contributors)
+    \n1. [${answers.Table_of_contents[9]}](#contribute)
     \n1. [${answers.Table_of_contents[5]}](#questions)
-    \n## ${answers.Table_of_contents[0]}
+    \n1. [${answers.Table_of_contents[2]}](#license)
+
+    \n## [${answers.Table_of_contents[0]}](#installation)
     \n${answers.installation}
-    \n## ${answers.Table_of_contents[1]}
+    \n## [${answers.Table_of_contents[1]}](#usage)
     \n${answers.usage}
-    \n<iframe src="https://drive.google.com/file/d/1Qp05CiAGjZm47CEd0F0o0-C7Hpe5Y7dI/preview" width="640" height="480"></iframe>
-    \n## ${answers.Table_of_contents[4]}
+    \n## [Walkthrough Video](https://drive.google.com/file/d/1Qp05CiAGjZm47CEd0F0o0-C7Hpe5Y7dI/preview)
+    \n## [${answers.Table_of_contents[4]}](#test)
     \n${answers.test}
-    \n## Badges
+    \n## [${answers.Table_of_contents[7]}](#badges)
     \n![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
     \nBadges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-    \n## Features
+    \n## [${answers.Table_of_contents[8]}](#features)
     \nIf your project has a lot of features, list them here.
-    \n## ${answers.Table_of_contents[3]}
+    \n## [${answers.Table_of_contents[3]}](#contributors)
     \n${answers.contributors}
-    \n## How to Contribute
+    \n## [${answers.Table_of_contents[9]}](#contribute)
     \nIf you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
-    \n## ${answers.Table_of_contents[5]}
+    \n## [${answers.Table_of_contents[5]}](#questions)
     \n${answers.questions}
-    \n## ${answers.Table_of_contents[2]}
+    \n## [${answers.Table_of_contents[2]}](#license)
     \n${answers.license}
-    `,"utf-8", function () {    
-            console.log("Succesfully wrote the file!");
+    `,"utf-8", function (error) {    
+        error ? console.error(error) : console.log("Succesfully wrote the file!");
         }
     )
 
